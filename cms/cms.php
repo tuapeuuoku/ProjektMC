70b1c42aa38aabc21538b8f5f744a8152c2c4e4e
tuapeuuoku <mikoc2004@wp.pl>
2023-02-15 14:17:01 +0100

    Baza

--- cms/cms.php
+++ cms/cms.php
@@ -19,6 +19,7 @@
     //sprawdź czy został wysłany formularz
     if(isset($_POST['submit'])) 
     {
+
         //zdefiniuj folder do którego trafią pliki (ścieżka względem pliku index.php)
         $targetDir = "image/";
 
@@ -71,44 +72,17 @@
 
 
         echo "Plik został poprawnie wgrany na serwer";
-    }
 
-    if(isset($_POST['submit']))
-{
-    $filename = $_FILES['uploadedFile']['name'];
-    $tempFileUrl = $_FILES["uploadedFile"]["tmp_name"];
-    $targetDir = "img/";
-    $imageInfo = getimagesize($_FILES["uploadedFile"]["tmp_name"]);
-    if(!is_array($imageInfo)) {
-        die("BŁĄD: Nieprawidłowy format obrazu");
+        $db = new mysqli("localhost", "root", "", "post");
+        $q = "INSERT post (ID, timestamp, filename) VALUES (NULL, ?, ?)";
+        $preparedQ = $db->prepare($q);
+        $date = date('Y-m-d H:i:s');
+        $preparedQ->bind_param('ss', $date, $filename);
+        $result = $preparedQ->execute();
+        if (!$result) {
+            die("Błąd bazy danych");
     }
- 
-    $imgString = file_get_contents($tempFileUrl);
-    $gdImage = imagecreatefromstring($imgString);
- 
-    $targetExtension = pathinfo($filename, PATHINFO_EXTENSION);
-    $targetExtension = strtolower($targetExtension);
- 
-    $filename = $filename . hrtime(true);
-    $filename = hash("sha256", $filename);
- 
-    $targetUrl = $targetDir . $filename . "." . $targetExtension;
-    if(file_exists($targetUrl))
-        die("BŁĄD: Plik o tej nazwie juz istnieje");
-    //move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $targetUrl);
-    $targetUrl = $targetDir . $filename . ".webp";
-    imagewebp($gdImage, $targetUrl);
- 
-    $db = new mysqli("localhost", "root", "", "cms");
-    $q = "INSERT post (ID, timestamp, filename) VALUES (NULL, ?, ?)";
-    $preparedQ = $db->prepare($q);
-    $date = date('Y-m-d H:i:s');
-    $preparedQ->bind_param('ss', $date, $filename);
-    $result = $preparedQ->execute();
-    if (!$result) {
-        die("Błąd bazy danych");
     }
-}
     ?>
 </body>
 </html>
