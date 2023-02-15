458bceb281dd779373650a295456b0a58efa9269
tuapeuuoku <mikoc2004@wp.pl>
2023-02-15 14:03:44 +0100

    laczenie

--- cms/cms.php
+++ cms/cms.php
@@ -16,15 +16,19 @@
     </form>
 
     <?php
+    //sprawdź czy został wysłany formularz
     if(isset($_POST['submit'])) 
     {
+        //zdefiniuj folder do którego trafią pliki (ścieżka względem pliku index.php)
+        $targetDir = "image/";
 
-        $targetDir = "img/";
-
+        //pobierz pierwotną nazwę pliku z tablicy $_FILES
         $sourceFileName = $_FILES['uploadedFile']['name'];
 
+        //pobierz tymczasową ścieżkę do pliku na serwerze
         $tempURL = $_FILES['uploadedFile']['tmp_name'];
 
+        //sprawdź czy mamy do czynienia z obrazem
         $imgInfo = getimagesize($tempURL);
         if(!is_array($imgInfo)) {
             die("BŁĄD: Przekazany plik nie jest obrazem!");
@@ -36,12 +40,16 @@
         //$sourceFileExtension = strtolower($sourceFileExtension);
         /// niepotrzebne - generujemy webp
 
+        //wygeneruj hash - nową nazwę pliku
         $newFileName = hash("sha256", $sourceFileName) . hrtime(true)
                             . ".webp";
 
         
+        //zaczytujemy cały obraz z folderu tymczasowego do stringa
         $imageString = file_get_contents($tempURL);
 
+        //generujemy obraz jako obiekt klasy GDImage
+        //@ przed nazwa funkcji powoduje zignorowanie ostrzeżeń
         $gdImage = @imagecreatefromstring($imageString);
 
         //wygeneruj pełny docelowy URL
@@ -64,6 +72,43 @@
 
         echo "Plik został poprawnie wgrany na serwer";
     }
+
+    if(isset($_POST['submit']))
+{
+    $filename = $_FILES['uploadedFile']['name'];
+    $tempFileUrl = $_FILES["uploadedFile"]["tmp_name"];
+    $targetDir = "img/";
+    $imageInfo = getimagesize($_FILES["uploadedFile"]["tmp_name"]);
+    if(!is_array($imageInfo)) {
+        die("BŁĄD: Nieprawidłowy format obrazu");
+    }
+ 
+    $imgString = file_get_contents($tempFileUrl);
+    $gdImage = imagecreatefromstring($imgString);
+ 
+    $targetExtension = pathinfo($filename, PATHINFO_EXTENSION);
+    $targetExtension = strtolower($targetExtension);
+ 
+    $filename = $filename . hrtime(true);
+    $filename = hash("sha256", $filename);
+ 
+    $targetUrl = $targetDir . $filename . "." . $targetExtension;
+    if(file_exists($targetUrl))
+        die("BŁĄD: Plik o tej nazwie juz istnieje");
+    //move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], $targetUrl);
+    $targetUrl = $targetDir . $filename . ".webp";
+    imagewebp($gdImage, $targetUrl);
+ 
+    $db = new mysqli("localhost", "root", "", "cms");
+    $q = "INSERT post (ID, timestamp, filename) VALUES (NULL, ?, ?)";
+    $preparedQ = $db->prepare($q);
+    $date = date('Y-m-d H:i:s');
+    $preparedQ->bind_param('ss', $date, $filename);
+    $result = $preparedQ->execute();
+    if (!$result) {
+        die("Błąd bazy danych");
+    }
+}
     ?>
 </body>
 </html>
