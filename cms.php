8b31ea3a054fd2b7663e783d64aee6fefec8affa
tuapeuuoku <mikoc2004@wp.pl>
2023-02-15 13:54:25 +0100

    body

---
+++ cms.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="uploadedFileInput">
+            Wybierz plik do wgrania na serwer:
+        </label><br>
+        <input type="file" name="uploadedFile" id="uploadedFileInput" required><br>
+        <input type="submit" value="Wyślij plik" name="submit"><br>
+    </form>
